cee5c0d90fb940da3c999293c5929364392211b9
廖韋臣 <rax02270521@gmail.com>
2020-10-31 21:50:36 +0800

    更改navigation.blade.php&index.blade.php的 a 標籤&測試

--- resources/views/layouts/partials/navigation.blade.php
+++ resources/views/layouts/partials/navigation.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="index.blade.php">Start Bootstrap</a>
+        <a class="navbar-brand" href="{{route('posts.index')}}">Start Bootstrap</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
@@ -8,16 +8,16 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.blade.php">Home</a>
+                    <a class="nav-link" href="{{route('posts.index')}}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="about.blade.php">About</a>
+                    <a class="nav-link" href="{{route('posts.about')}}">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="post.blade.php">Sample Post</a>
+                    <a class="nav-link" href="{{route('posts.show')}}">Sample Post</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="contact.blade.php">Contact</a>
+                    <a class="nav-link" href="{{route('posts.contact')}}">Contact</a>
                 </li>
             </ul>
         </div>
