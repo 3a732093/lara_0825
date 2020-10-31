6aad5a7a50b56418e3a8bf9d12b425befd1fb767
廖韋臣 <rax02270521@gmail.com>
2020-10-31 20:53:56 +0800

    修改子樣板 index.blade.php套用主樣板&修改錯誤&測試根路由

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,10 +41,10 @@
 
 <!-- Bootstrap core JavaScript -->
 <script src="{{asset('js/jquery.min.js')}}"></script>
-<script src="{{asset('js/bootstrap.bundle.min.js'}}"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- Custom scripts for this template -->
-<script src="{{asset('js/clean-blog.min.js'}}"></script>
+<script src="{{asset('js/clean-blog.min.js')}}"></script>
 
 </body>
 
