eb588ea17c9cc18ef520c160ee1106b34920c5e2
廖韋臣 <rax02270521@gmail.com>
2020-11-16 19:40:19 +0800

    練習7-2 增加關聯Post->Comment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Comment;
 class Post extends Model
 {
     use HasFactory;
@@ -16,4 +16,9 @@ class Post extends Model
            'is_feature'
 
     ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
