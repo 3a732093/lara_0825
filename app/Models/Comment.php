8ac1fbc03f98369161a4c81e24189ccea653a297
廖韋臣 <rax02270521@gmail.com>
2020-11-16 19:43:02 +0800

    練習7-3 增加關聯Comment->Post

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,7 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Post;
 
 class Comment extends Model
 {
@@ -14,4 +14,9 @@ class Comment extends Model
     protected $fillable =[
         'content'
     ];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
